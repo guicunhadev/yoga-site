1335faaf8eee7bf1ed08d84e06d787f0b01925b4
guicunhadev <guilhermealmeida2008@hotmail.com>
2023-12-21 12:42:22 -0300

    changes em images and etc...

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+// -------------Cookies----------------
+
+var msgCookies = document.getElementById('cookies-msg')
+
+function aceito(){
+    localStorage.lgpd = "sim"
+    msgCookies.classList.remove('mostrar')
+}
+
+if(localStorage.lgpd == 'sim'){
+    msgCookies.classList.remove('mostrar')
+}else{
+    msgCookies.classList.add('mostrar')
+}
+
+
+
+
+
 //----------------  Side Menu small screen----------------------
 function showSidebar(){
     const sidebar = document.querySelector('.sidebar')
